87b3227c8b4933343014ac2c9851dc9d15a28ae5
Lucas Chalub <chalubaol9@gmail.com>
2021-08-25 20:58:13 -0300

    create seeder

---
+++ database/migrations/2021_08_25_225431_create_contacts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('contactable');
+            $table->string('phone_number', 11);
+            $table->boolean('whatsapp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
